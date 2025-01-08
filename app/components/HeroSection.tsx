2dbff2c1810076cd942761e53e9937470121aa5d
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-08 21:49:42 +0530

    init herosection

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+export default function HeroSection() {
+    return(
+        <div className="relative overflow-hidden py-12 sm:py-16 md:py-20 lg:py-24 xl:py-28">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl md:text-5xl lg:text-6xl">
+                Hi, I&apos;m Chaitanya
+            </h1>
+            <p className="mt-4 sm:mt-6 text-base sm:text-lg md:text-xl leading-7 sm:leading-8 text-gray-600 dark:text-gray-300">
+            Full Stack Developer | AI Enthusiast | Problem Solver
+            </p>
+        </div>
+    )
+}
