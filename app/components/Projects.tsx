94139b4e603e8cbc053e2b2887a36056a88a46d5
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-10 18:48:16 +0530

    init project page

---
+++ app/components/Projects.tsx
@@ -0,0 +1,9 @@
+export default function Project(){
+    return (
+        <section>
+            <div>
+                <h1>Projects</h1>
+            </div>
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,14 @@ import AboutMe from "./components/AboutMe";
 import Experience from "./components/Experience";
 import HeroSection from "./components/HeroSection";
 import { AnimatePresence } from "framer-motion";
+import Project from "./components/Projects";
 export default function Home(){
     return(
         <AnimatePresence mode="wait">
             <HeroSection key="home" />
             <AboutMe key="about" />
             <Experience key="experience"/>
+            <Project key="projects"/>
         </AnimatePresence>
     )
 }
