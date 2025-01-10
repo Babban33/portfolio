3870786efb330a214a65e524383b8062de1a126d
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-10 11:30:33 +0530

    added images api

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.leetcode.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'files.codingninjas.in'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
