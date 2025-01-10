703d412978d5a797c354e973b9891e8d9765b9f9
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-10 19:12:11 +0530

    fix: h1 to h2

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -29,14 +29,14 @@ export default function Project(){
             className="py-20 sm:py-32"
         >
             <div className="container mx-auto max-w-screen-xl">
-                <motion.h1 
+                <motion.h2 
                     initial={{ opacity: 0, y: -20 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.5, delay: 0.2 }}
                     className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl mb-12 text-center"
                 >
                     Projects
-                </motion.h1>
+                </motion.h2>
             </div>
         </motion.section>
     )
