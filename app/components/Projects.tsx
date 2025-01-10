2c9bbe1c5f352ace52bd1ceb060d20341e163b23
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-10 18:50:22 +0530

    added heading

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,8 +1,8 @@
 export default function Project(){
     return (
-        <section>
-            <div>
-                <h1>Projects</h1>
+        <section id="projects" className="py-20 sm:py-32">
+            <div className="container mx-auto max-w-screen-xl">
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl mb-12 text-center">Projects</h1>
             </div>
         </section>
     )
