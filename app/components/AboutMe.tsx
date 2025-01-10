50a6d7aa0e7ddf3c998b9121d6c25df682e3428d
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-10 10:42:59 +0530

    init aboutme section

---
+++ app/components/AboutMe.tsx
@@ -0,0 +1,9 @@
+export default function AboutMe() {
+    return(
+        <section className="bg-muted/50">
+            <div className="container">
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl mb-12 text-center"> About Me</h2>
+            </div>
+        </section>
+    )
+}
