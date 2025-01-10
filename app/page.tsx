f501156912beeb0f9341ed0e8ee577d74a13904e
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-10 10:47:26 +0530

    added about section and keys for children

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import AboutMe from "./components/AboutMe";
 import HeroSection from "./components/HeroSection";
 import { AnimatePresence } from "framer-motion";
 export default function Home(){
     return(
         <AnimatePresence mode="wait">
-            <HeroSection />
+            <HeroSection key="home" />
+            <AboutMe key="about" />
         </AnimatePresence>
     )
 }
