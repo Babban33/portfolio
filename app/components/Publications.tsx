b931966eb5d600a47e003015a87dfd1e2c136135
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-12 15:15:13 +0530

    added transition on heading

--- app/components/Publications.tsx
+++ app/components/Publications.tsx
@@ -11,7 +11,15 @@ export default function Publications(){
             className="py-16 bg-muted/30"
         >
             <div className="container max-w-xl mx-auto">
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl mb-12 text-center">Publications</h2>
+                <motion.h2 
+                    className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl mb-12 text-center"
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -20 }}
+                    transition={{ duration: 0.5, delay: 0.2 }}
+                >
+                    Publications
+                </motion.h2>
             </div>
         </motion.section>
     )
