f98e8aa555f05a4d71cb154cae64988c8ed7d616
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-12 13:02:27 +0530

    add: category buttons

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,6 +1,19 @@
+"use client"
+import { Button } from "@/components/ui/button";
 import {motion} from "framer-motion";
+import { Code, Server, Database, Cloud, Brain } from 'lucide-react'
+import { useState } from "react";
+
+const skillCategories = [
+  { name: "Frontend", icon: Code },
+  { name: "Backend", icon: Server },
+  { name: "Databases", icon: Database },
+  { name: "DevOps", icon: Cloud },
+  { name: "AI/ML", icon: Brain }
+]
 
 export default function Skills(){
+    const [activeCategory, setActiveCategory] = useState("Frontend");
     return(
         <section className="py-16 sm:py-32 bg-muted/50">
             <div className="container max-w-screen-xl mx-auto">
@@ -12,6 +25,21 @@ export default function Skills(){
                 >
                     Skills
                 </motion.h2>
+
+                {/* Catergory buttons */}
+                <div className="flex flex-wrap justify-center mb-8 gap-4">
+                    {skillCategories.map((category) => (
+                        <Button
+                            key={category.name}
+                            variant={activeCategory === category.name ? "default" : "outline"}
+                            onClick={() => setActiveCategory(category.name)}
+                            className="flex items-center"
+                        >
+                            <category.icon className="mr-2" size={16} />
+                            {category.name}
+                        </Button>
+                    ))}
+                </div>
             </div>
         </section>
     )
