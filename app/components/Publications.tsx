e37dcfe5e059d80c4de826b736bf0b601af4570b
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-12 15:13:46 +0530

    added transition on section

--- app/components/Publications.tsx
+++ app/components/Publications.tsx
@@ -1,12 +1,18 @@
+import { motion } from "framer-motion";
+
 export default function Publications(){
     return(
-        <section
+        <motion.section
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1}}
+            exit={{ opacity: 0, y: -50 }}
+            transition={{ duration: 0.5 }}
             id="publications" 
             className="py-16 bg-muted/30"
         >
             <div className="container max-w-xl mx-auto">
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl mb-12 text-center">Publications</h2>
             </div>
-        </section>
+        </motion.section>
     )
 }
